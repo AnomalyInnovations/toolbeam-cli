e5cd59c3150e6ba2c8e2220a77614a273707abdf
Fanjie Wang <frank@anomalyinnovations.com>
2016-09-15 20:01:16 -0400

    Updated config file

--- src/config.js
+++ src/config.js
@@ -1,12 +1,8 @@
 export default {
 	webScheme: 'https',
 	webHost: 'toolbeam.com',
-// TODO
 	apiScheme: 'https',
 	apiHost: 'api.toolbeam.com',
 	apiPort: 443,
-//	apiScheme: 'http',
-//	apiHost: 'api.toolbeam.dev',
-//	apiPort: 80,
 	specFileName: 'spec.json',
 };
