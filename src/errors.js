e802df9148d23e470199a51d32f4079375490d02
Jay <jvelayud@gmail.com>
2016-09-27 20:32:26 -0400

    Removing clui from deps

--- src/errors.js
+++ src/errors.js
@@ -10,9 +10,11 @@ export const ERR_ADD_MISSING_PARAM_NAME = 'Missing parameter name';
 export const ERR_ADD_MISSING_PARAM_IN = 'Missing parameter in';
 export const ERR_ADD_SPEC_NOT_EXISTS = 'Spec file does not exist. Run tb init first';
 export const ERR_ADD_TOOL_EXISTS = 'Tool with the same operation and path already exist';
+
 export const ERR_PULL_UUID_DOES_NOT_MATCH = 'UUID provided does not match that in the current project';
 export const ERR_PULL_UUID_NOT_PROVIDED = 'Please specify a project UUID';
 export const ERR_PULL_PARSE_SPEC_JSON = 'Could not parse project file for UUID due to invalid json';
 export const ERR_PULL_UUID_NOT_IN_SPEC = 'Found an existing project in the current directory, but the project does not contain a project UUID. Push the project first to acquire a UUID';
+
 export const ERR_REMOVE_SPEC_NOT_EXISTS = 'Spec file does not exist';
 export const ERR_REMOVE_PARSE_SPEC_JSON = 'Could not parse project file for the tool due to invalid json';
